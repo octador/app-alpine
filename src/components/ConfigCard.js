572e01bfc0d847544c91ae221d4021fe92fe86a4
Mutuon Joan <mutuon.j@gmail.com>
2024-08-30 18:51:10 +0200

    delet feacture configcar

--- src/components/ConfigCard.js
+++ src/components/ConfigCard.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Color from "./Color";
-import Rims from "./Rims";
-import Sealing from "./Sealing";
+
 
 function ConfigCard({ choiceCar }) {
   let priceTotal = [];
