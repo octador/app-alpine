9f493a952495acc6a4bdd6ab4cb71d9af84e7fd4
Mutuon Joan <mutuon.j@gmail.com>
2024-08-30 18:55:00 +0200

    remove function configcar

--- src/components/ConfigCard.js
+++ src/components/ConfigCard.js
@@ -1,34 +1,32 @@
 import React from "react";
 
-
 function ConfigCard({ choiceCar }) {
   let priceTotal = [];
 
   console.log(priceTotal);
 
   priceTotal.push(choiceCar.price);
-
   priceTotal.push(choiceCar.rims.price);
   priceTotal.push(choiceCar.color.price);
   priceTotal.push(choiceCar.Sealing.price);
   priceTotal.push(choiceCar.mediaAndNavigation.price);
 
-  choiceCar.equipment.design.map((design) => {
+  choiceCar.equipment.design.forEach((design) => {
     priceTotal.push(design.price);
   });
-  choiceCar.equipment.comfort.map((comfort) => {
+  choiceCar.equipment.comfort.forEach((comfort) => {
     priceTotal.push(comfort.price);
   });
-  choiceCar.equipment.conduct.map((conduct) => {
+  choiceCar.equipment.conduct.forEach((conduct) => {
     priceTotal.push(conduct.price);
   });
-  choiceCar.equipment.security.map((security) => {
+  choiceCar.equipment.security.forEach((security) => {
     priceTotal.push(security.price);
   });
-  choiceCar.equipment.exteriorCustomization.map((exteriorCustomization) => {
+  choiceCar.equipment.exteriorCustomization.forEach((exteriorCustomization) => {
     priceTotal.push(exteriorCustomization.price);
   });
-  choiceCar.equipment.interiorCustomization.map((interiorCustomization) => {
+  choiceCar.equipment.interiorCustomization.forEach((interiorCustomization) => {
     priceTotal.push(interiorCustomization.price);
   });
 
@@ -36,16 +34,16 @@ function ConfigCard({ choiceCar }) {
   console.log(totalPrice);
 
   return choiceCar ? (
-    <div class="dropup-center dropup fixed-bottom mt-5">
+    <div className="dropup-center dropup fixed-bottom mt-5">
       <button
-        class="btn btn-secondary dropdown-toggle p-2 text-lg"
+        className="btn btn-secondary dropdown-toggle p-2 text-lg"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
       >
         {"Total price " + totalPrice + " Euros "}
       </button>
-      <ul class="dropdown-menu">
+      <ul className="dropdown-menu">
         <li className="py-3 px-2">Version : {choiceCar.version}</li>
         <hr />
         <li className="py-3 px-2">
@@ -61,72 +59,52 @@ function ConfigCard({ choiceCar }) {
         </li>
         <hr />
         <li className="py-3 px-2">Design : </li>
-        {choiceCar.equipment.design.map((design) => {
-          return (
-            <li>
-              {" "}
-              - {design.name} price : {design.price} €{" "}
-            </li>
-          );
-        })}
+        {choiceCar.equipment.design.map((design) => (
+          <li key={design.name}>
+            {" "}
+            - {design.name} price : {design.price} €{" "}
+          </li>
+        ))}
         <hr />
         <li className="py-3 px-2">Comfort : </li>
-        {choiceCar.equipment.comfort.map((comfort) => {
-          return (
-            <li>
-              {" "}
-              - {comfort.name} price : {comfort.price} €{" "}
-            </li>
-          );
-        })}
+        {choiceCar.equipment.comfort.map((comfort) => (
+          <li key={comfort.name}>
+            {" "}
+            - {comfort.name} price : {comfort.price} €{" "}
+          </li>
+        ))}
         <hr />
         <li className="py-3 px-2">Conduct : </li>
-        {choiceCar.equipment.conduct.map((conduct) => {
-          return (
-            <li>
-              {" "}
-              - {conduct.name} price : {conduct.price} €{" "}
-            </li>
-          );
-        })}
+        {choiceCar.equipment.conduct.map((conduct) => (
+          <li key={conduct.name}>
+            {" "}
+            - {conduct.name} price : {conduct.price} €{" "}
+          </li>
+        ))}
         <hr />
         <li className="py-3 px-2">Security : </li>
-        {choiceCar.equipment.security.map((security) => {
-          return (
-            <li>
-              {" "}
-              - {security.name} price : {security.price} €{" "}
-            </li>
-          );
-        })}
+        {choiceCar.equipment.security.map((security) => (
+          <li key={security.name}>
+            {" "}
+            - {security.name} price : {security.price} €{" "}
+          </li>
+        ))}
         <hr />
-
         <li className="py-3 px-2">Exterior Customization : </li>
-        {choiceCar.equipment.exteriorCustomization.map(
-          (exteriorCustomization) => {
-            return (
-              <li>
-                {" "}
-                - {exteriorCustomization.name} price :{" "}
-                {exteriorCustomization.price} €{" "}
-              </li>
-            );
-          }
-        )}
+        {choiceCar.equipment.exteriorCustomization.map((exteriorCustomization) => (
+          <li key={exteriorCustomization.name}>
+            {" "}
+            - {exteriorCustomization.name} price : {exteriorCustomization.price} €{" "}
+          </li>
+        ))}
         <hr />
-
-        <li className="py-3 px-2">Intérior Customization: </li>
-        {choiceCar.equipment.interiorCustomization.map(
-          (interiorCustomization) => {
-            return (
-              <li>
-                {" "}
-                - {interiorCustomization.name} price :{" "}
-                {interiorCustomization.price} €{" "}
-              </li>
-            );
-          }
-        )}
+        <li className="py-3 px-2">Interior Customization: </li>
+        {choiceCar.equipment.interiorCustomization.map((interiorCustomization) => (
+          <li key={interiorCustomization.name}>
+            {" "}
+            - {interiorCustomization.name} price : {interiorCustomization.price} €{" "}
+          </li>
+        ))}
       </ul>
     </div>
   ) : null;
